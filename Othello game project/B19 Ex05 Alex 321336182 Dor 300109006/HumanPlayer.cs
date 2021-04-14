ba85597d15cd3693501406e9d33006d07dc0af1f
Dor <dorsharoni10@gmail.com>
2021-04-14 14:34:30 +0300

    Update HumanPlayer.cs

--- Othello game project/B19 Ex05 Alex 321336182 Dor 300109006/HumanPlayer.cs
+++ Othello game project/B19 Ex05 Alex 321336182 Dor 300109006/HumanPlayer.cs
@@ -8,7 +8,7 @@ namespace B19_Ex05_Alex_321336182_Dor_300109006
         public HumanPlayer(string i_Name, Board.eCellContent i_DiskType)
         {
             Name = i_Name;
-            DiskType = i_DiskType;
+            DiskType = i_DiskType
         }
 
         public string Name { get; }
